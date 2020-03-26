cada1367dcf6885b03c9698d38961f5a8a1e9f46
Evgeniy Lobanov <dirtha@yandex.ru>
2020-03-26 16:41:15 +0200

    Interim commit

--- src/index.js
+++ src/index.js
@@ -18,36 +18,60 @@ import { NewsCardList } from './js/modules/NewsCardList.js';
 
     function showLoading(isLoad) {
         if (isLoad) {
-            document.querySelector('.results').style.display = 'block';
-            document.querySelector('.loader').style.display = 'block';
             document.querySelector('.content-subtitle_load').textContent = 'Идёт загрузка...';
             document.querySelector('.content-subtitle_load').style.display = 'block';
-            document.querySelector('.results__wrapper').style.display = 'none';
+            document.querySelector('.loader').style.display = 'block';
+            document.querySelector('.results').style.display = 'block';
         } else {
-            document.querySelector('.loader').style.display = 'none';
             document.querySelector('.content-subtitle_load').style.display = 'none';
-            document.querySelector('.results__wrapper').style.display = 'block';
+            document.querySelector('.loader').style.display = 'none';
         }
+    }
+
+    function showResults(isFound) {
+        if (isFound) {
+            document.querySelector('.results__wrapper').style.display = 'block';
+        } else {
+            document.querySelector('.results__not-found-image').style.display = 'block';
+            document.querySelector('.content-title_load').style.display = 'block';
+            document.querySelector('.content-subtitle_load').textContent = 'К сожалению по вашему запросу ничего не найдено';
+            document.querySelector('.content-subtitle_load').style.display = 'block';
+        }
+    }
+
+    function updateResults() {
+        document.querySelector('.results__wrapper').style.display = 'none';
+        document.querySelector('.results__not-found-image').style.display = 'none';
+        document.querySelector('.content-title_load').style.display = 'none';
+        document.querySelector('.content-subtitle_load').style.display = 'none';
     }
 
     function searchHandler(event) {
         event.preventDefault();
         sessionStorage.clear();
+        updateResults();
         showLoading(true);
         requestToApi.getNews(searchForm.elements.topic.value, '2020-03-25',
             (data) => {
-                data.articles.forEach((item, index) => {
-                    sessionStorage.setItem(index, JSON.stringify(item));
-                });
-                resultsList.renderCardList(true, document.createElement('div'), 
-                    (article) => card.create(article.title, article.description, article.publishedAt, article.source.name, article.urlToImage, article.url)
-                );
-                showLoading(false);
+                if (data.articles.length !== 0) {
+                    data.articles.forEach((item, index) => {
+                        sessionStorage.setItem(index, JSON.stringify(item));
+                    });
+                    console.log(sessionStorage.length);
+                    resultsList.renderCardList(true, document.createElement('div'), moreBtn, 
+                        (article) => card.create(article.title, article.description, article.publishedAt, article.source.name, article.urlToImage, article.url)
+                    );
+                    showLoading(false);
+                    showResults(true);
+                } else {
+                    showLoading(false);
+                    showResults(false);
+                }
             });
     }
 
     function moreNewsHandler(event) {
-        resultsList.renderCardList(false, document.createElement('div'), 
+        resultsList.renderCardList(false, document.createElement('div'), moreBtn, 
             (article) => card.create(article.title, article.description, article.publishedAt, article.source.name, article.urlToImage, article.url)
         );
     }
--- src/js/modules/NewsCardList.js
+++ src/js/modules/NewsCardList.js
@@ -5,15 +5,22 @@ export class NewsCardList {
     this.start = 0;
   }
 
-  renderCardList(firstRender, divContainer, createCardMethod) {
-    if (firstRender) {
+  renderCardList(isFirst, divContainer, downloadMoreButton, createCardMethod) {
+    if (isFirst) {
       this.cardsContainer.replaceWith('');
       this.start = 0;
       this.cardsContainer = divContainer;
       this.cardsContainer.classList.add('results__news-cards');
       this.previousElem.after(this.cardsContainer);
+      downloadMoreButton.style.display = 'block';
     }
-    for (let i = this.start; i < this.start + 3; i++) {
+    for (let i = this.start; i < this.start + 3; i++) {;
+      if (sessionStorage.getItem(i) === null) {
+        break;
+      }
+      if (sessionStorage.getItem(i + 1) === null) {
+        downloadMoreButton.style.display = 'none';
+      }
       this.cardsContainer.append((createCardMethod(JSON.parse(sessionStorage.getItem(i)))));
     }
     this.start += 3;
